0e939cbb72ede39078da8db863696083705d4be8
Eric Watjen <ewatjen@gmail.com>
2015-06-24 16:05:31 -0700

    Initial Commit

---
+++ PostcardUITests/PostcardUITests.swift
@@ -0,0 +1,35 @@
+//
+//  PostcardUITests.swift
+//  PostcardUITests
+//
+//  Created by Eric Watjen on 6/24/15.
+//  Copyright © 2015 Eric Watjen. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class PostcardUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
